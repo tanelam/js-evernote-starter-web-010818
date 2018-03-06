11c64a653e91c5e9997f35745f487667e0f4c517
tanelam <tania.e.aparicio@gmail.com>
2018-03-06 15:52:56 -0500

    done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show,:update,:destroy]
 
   def index
-    users = User.all
-    render json: users, status: 200
+    @users = User.all
+    render json: @users, status: 200
   end
 
   def create
-    user = User.create(user_params)
-    render json: user, status: 201
+    @user = User.create(user_params)
+    render json: @user, status: 201
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :notes
-      resources :users
+      resources :notes, only: [:index, :create, :update, :destroy, :show]
+      resources :users, only: [:index, :create, :update, :destroy, :show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
